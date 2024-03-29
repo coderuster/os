0911c68c08377f0a1b5f44476e03ac5286b2b8e0
coderuster <165394336+coderuster@users.noreply.github.com>
2024-03-29 21:51:40 +0530

    Delete os directory

--- os/7/b.sh
+++
@@ -1,4 +0,0 @@
-b. Execution of getpid and close Commands in UNIX
-getpid: The getpid command in UNIX is not directly executable from the command line. It is a system call used in programming to get the process ID of the current process.
-
-close: The close command in UNIX is used to close a file descriptor or file stream.
